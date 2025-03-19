200983f6677d06a525bd6f7b7d18fee6ea80ab45
Alessandro <117670730+AlessandroBocquet@users.noreply.github.com>
2025-03-19 11:56:18 +0100

    Fix sur google chrome des images et de leur wrap
    Pour l'instant j'ai que fix en mettant une height max

--- index.js
+++ index.js
@@ -78,3 +78,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 300); 
     });
 });
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementById("dropdown-content");
+  dropdownContent.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
